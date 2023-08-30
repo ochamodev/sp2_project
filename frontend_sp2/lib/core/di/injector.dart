cbc63b769e5ba74f1910a294579f719e028febb4
ochamodev <cheleyotto98@gmail.com>
2023-08-29 22:48:40 -0600

    feat: setup dio initial config

--- frontend_sp2/lib/core/di/injector.dart
+++ frontend_sp2/lib/core/di/injector.dart
@@ -1,4 +1,5 @@
 
+import 'package:dio/dio.dart';
 import 'package:frontend_sp2/core/navigation/app_router.dart';
 import 'package:get_it/get_it.dart';
 import 'package:logger/logger.dart';
@@ -9,5 +10,14 @@ Future<void> initializeInjectedDependencies() async {
 
   getIt.registerLazySingleton(() => Logger());
   getIt.registerSingleton(AppRouter());
+  getIt.registerSingleton(setupDio());
 
+}
+
+Dio setupDio() {
+  final dio = Dio();
+  dio.options.baseUrl = '127.0.0.1:5000/';
+  dio.options.connectTimeout = const Duration(seconds: 5);
+  dio.options.receiveTimeout = const Duration(seconds: 5);
+  return dio;
 }
